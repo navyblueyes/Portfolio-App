28a8bed228186ce9607570d009107c6fd33e01d5
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-12-01 18:05:36 -0600

    fixed dependencies

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Profile() {
+  return (
+    <div classname="profile">
+      <div classname="profile-parent">
+        <div classname="colz">
+          <a href="https://www.linkedin.com/in/michael-m-1a0131195/">
+            <i classname="fab fa-linkedin-in"></i>
+          </a>
+          <a href="https://www.credly.com/users/michael-minkoff.21fd5056">
+            <i classname="fas fa-copyright"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
