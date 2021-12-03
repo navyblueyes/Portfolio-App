cc10dbebc8611c890be78b019dbb4ced72092754
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-12-03 16:16:19 -0600

    header; started on UI of header

--- src/PortfolioContainer/Home/Header/Header.js
+++ src/PortfolioContainer/Home/Header/Header.js
@@ -6,6 +6,7 @@ import {
 import ScrollService from "../../../utilities/ScrollService";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import MinkLogo from "../../../../public/MikeMinkoff-logo.svg";
 import "./Header.css";
 
 export default function Header() {
@@ -20,5 +21,62 @@ export default function Header() {
   let currentScreenSubscription =
     ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
 
-  return <div></div>;
+  const getHeaderOptions = () => {
+    return TOTAL_SCREENS.map((screen, i) => (
+      <div
+        key={screen.screen_name}
+        className={getHeaderOptionsClass(i)}
+        onClick={() => switchScreen(i, screen)}
+      >
+        <span>{screen.screen_name}</span>
+      </div>
+    ));
+  };
+
+  const getHeaderOptionsClass = (index) => {
+    let classes = "header-option";
+    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-separator";
+    if (selectedScreen === index) {
+      classes += "selected-header-option";
+      return;
+    }
+  };
+
+  const switchScreen = (index, screen) => {
+    let screenComponent = document.getElementById(screen.screen_name);
+    if (!screenComponent) return;
+    screenComponent.scrollIntoView({ behavior: "smooth" });
+    setSelectedScreen(index);
+    setShowHeaderOptions(false);
+  };
+
+  return (
+    <div>
+      <div
+        className="header-option"
+        onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+      >
+        <div className="header-parent">
+          <div
+            className="header-hamburger"
+            onClick={() => setShowHeaderOptions(!showHeaderOptions)}
+          >
+            <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
+          </div>
+          <div className="header-logo">
+            <MinkLogo />
+          </div>
+          <div
+            className={
+              showHeaderOptions
+                ? "header-options show-hamburger-options"
+                : "header-options"
+            }
+          >
+            {getHeaderOptions()}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
