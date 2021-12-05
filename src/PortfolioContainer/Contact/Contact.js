9c4229575ea90d3da6e0e84e20ea88eb324a6da3
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-12-05 16:03:39 -0600

    contact; forms started

--- src/PortfolioContainer/Contact/Contact.js
+++ src/PortfolioContainer/Contact/Contact.js
@@ -1,7 +1,10 @@
 import "./Contact.css";
+import React, { useState } from "react";
 import Typical from "react-typical";
-import React from "react";
-import imgGb from "../../../src/assets/ContactMe/mailz.jpeg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+import imgBg from "../../../src/assets/ContactMe/mailz.jpeg";
 import loader1 from "../../../src/assets/ContactMe/load2.gif";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollServices";
@@ -15,6 +18,22 @@ export default function Contact(props) {
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [banner, setBanner] = useState("");
+  const [bool, setBool] = useState(false);
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handleMessage = (e) => {
+    setMessage(e.target.value);
+  };
+  console.log(name);
+
   return (
     <div className="main-container" id={props.id || ""}>
       <ScreenHeading subHeading={"Let's Keep In Touch"} title={"Contact Me"} />
@@ -26,6 +45,29 @@ export default function Contact(props) {
           <a href="https://www.linkedin.com/in/michael-m-1a0131195/">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
+        </div>
+        <div className="back-form">
+          <div className="img-back">
+            <h4>Send Your Email Here!</h4>
+            <img src={imgBg} alt="image not found" />
+          </div>
+          <form>
+            <p>{banner}</p>
+            <label htmlFor="name">Name</label>
+            <input type="text" onChange={handleName} value={name} />
+
+            <label htmlFor="email">Email</label>
+            <input type="text" onChange={handleEmail} value={email} />
+
+            <label htmlFor="message">Message</label>
+            <textarea type="text" onChange={handleMessage} value={message} />
+
+            <div className="send-btn">
+              <button type="submit">
+                send <i className="fa fa-paper-plane" />
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
