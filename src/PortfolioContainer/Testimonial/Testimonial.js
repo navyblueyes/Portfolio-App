0349bda35c1ae6852dea888a67375704de11cc89
Michael Victor Minkoff <lock_ed_on@yahoo.com>
2021-12-05 13:29:58 -0600

    testimonial; photos perfected

--- src/PortfolioContainer/Testimonial/Testimonial.js
+++ src/PortfolioContainer/Testimonial/Testimonial.js
@@ -22,7 +22,154 @@ export default function Testimonial(props) {
         title={"Testimonial"}
         subHeading={"What My Client Say About Me"}
       />
-      <section className="testimonial-secriont" id={props.id || ""}></section>
+      <section className="testimonial-secriont" id={props.id || ""}>
+        <div className="container">
+          <div className="row">
+            <OwlCarousel className="owl-carousel" id="testimonial-carousel">
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      Mike delivers projects ontime. His professionalism shines
+                      in his work and maintained excellent communication
+                      throughout his work.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt"></i>
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src="img/testimonial/testil1.jpg" alt="no internet" />
+                    <h5>Haley Roman</h5>
+                    <p>CEO InansGlobal</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      Mike delivers projects ontime. His professionalism shines
+                      in his work and maintained excellent communication
+                      throughout his work.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt"></i>
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src="img/testimonial/testil2.jpg" alt="no internet" />
+                    <h5>Haley Roman</h5>
+                    <p>CEO InansGlobal</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      Mike delivers projects ontime. His professionalism shines
+                      in his work and maintained excellent communication
+                      throughout his work.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt"></i>
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src="img/testimonial/testil3.jpg" alt="no internet" />
+                    <h5>Haley Roman</h5>
+                    <p>CEO InansGlobal</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-12">
+                <div className="testi-item">
+                  <div className="testi-comment">
+                    <p>
+                      <i className="fa fa-quote-left" />
+                      Mike delivers projects ontime. His professionalism shines
+                      in his work and maintained excellent communication
+                      throughout his work.
+                      <i className="fa fa-quote-right" />
+                    </p>
+                    <ul className="stars list-unstyled">
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                      <li>
+                        <i className="fa fa-star-half-alt"></i>
+                      </li>
+                      <li>
+                        <i className="fa fa-star" />
+                      </li>
+                    </ul>
+                  </div>
+                  <div className="client-info">
+                    <img src="img/testimonial/testil4.jpg" alt="no internet" />
+                    <h5>Haley Roman</h5>
+                    <p>CEO InansGlobal</p>
+                  </div>
+                </div>
+              </div>
+            </OwlCarousel>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
